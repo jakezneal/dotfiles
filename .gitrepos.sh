fdaa37a65402d8972db26af344e18a8c17d9f951
Jake Neal <jakeneal@Jakes-MacBook-Pro.local>
2017-03-09 11:37:45 +0000

    Install Oh My Zsh

--- .gitrepos.sh
+++ .gitrepos.sh
@@ -4,3 +4,5 @@
 # {iterm2colorschemes.com}
 git clone https://github.com/mbadolato/iTerm2-Color-Schemes ~/iTerm2-Color-Schemes
 
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
