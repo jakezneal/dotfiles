e015a7405b16e204713ade065861406617f149cf
Jake Neal <hello@jakeneal.co.uk>
2017-03-09 14:29:55 +0000

    Add in Autoclose and Blade

--- .apm.sh
+++ .apm.sh
@@ -4,8 +4,10 @@
 apm install seti-ui
 
 # Plugins
+apm install autoclose-html
 apm install docblockr
 apm install file-icons
+apm install language-blade
 apm install minimap
 apm install open-in-browser
 apm install pigments
