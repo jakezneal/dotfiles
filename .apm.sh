6df0fd1b0995ca7ee6d77736df46d459f04398ed
Jake Neal <jakeneal@Jakes-MacBook-Pro.local>
2017-03-09 14:16:04 +0000

    Install Atom-related packages

---
+++ .apm.sh
@@ -0,0 +1,12 @@
+# Install Atom theme and plugins
+
+# Theme
+apm install seti-ui
+
+# Plugins
+apm install docblockr
+apm install file-icons
+apm install minimap
+apm install open-in-browser
+apm install pigments
+apm install qolor
