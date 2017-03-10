a0b20e6387f01f3f505e231a08f83f0e7af60fb5
Jake Neal <hello@jakeneal.co.uk>
2017-03-10 12:36:42 +0000

    Add in iTerm2 config

--- .init.sh
+++ .init.sh
@@ -1,5 +1,8 @@
 # Set some sensible defaults for installed apps
 
+# Copy iTerm2 config file
+cp init/com.googlecode.iterm2.plist ~/Library/Preferences
+
 # Don’t display the annoying prompt when quitting iTerm
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 
