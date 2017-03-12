73278f6519d0c3795f900072e6952146e9ff6749
Jake Neal <jake@wearethegarden.co.uk>
2017-03-12 21:29:20 +0000

    Remove duplicate Solarized Theme

--- .init.sh
+++ .init.sh
@@ -6,8 +6,5 @@ cp init/com.googlecode.iterm2.plist ~/Library/Preferences
 # Don’t display the annoying prompt when quitting iTerm
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 
-# Install the Solarized Dark theme for iTerm
-open "${HOME}/iTerm2-Color-Schemes/schemes/Solarized Dark.itermcolors"
-
 # Copy Atom config file
 cp init/config.cson ~/.atom
--- .misc.sh
+++ .misc.sh
@@ -1,9 +1,5 @@
 # Clone git repos & install misc tools
 
-# iTerm colour schemes
-# {iterm2colorschemes.com}
-git clone https://github.com/mbadolato/iTerm2-Color-Schemes ~/iTerm2-Color-Schemes
-
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
