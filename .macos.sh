3bffba796156b6067359b5b8b0572078a522e619
Jake Neal <jake@wearethegarden.co.uk>
2017-03-13 20:33:15 +0000

    Enable full keyboard access

--- .macos.sh
+++ .macos.sh
@@ -73,6 +73,9 @@ defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 # Enable dark theme
 defaults write ~/Library/Preferences/.GlobalPreferences AppleInterfaceStyle -string "Dark"
 
+# Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
 ###############################################################################
 # SSD-specific tweaks                                                         #
 ###############################################################################
