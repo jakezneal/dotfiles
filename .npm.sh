b3c38ac4c205b8224df1262a57570d5a66733faf
Jake Neal <jake@wearethegarden.co.uk>
2017-03-15 09:01:30 +0000

    Install Gulp 4 globally

--- .npm.sh
+++ .npm.sh
@@ -6,5 +6,5 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-npm i gulp -g
+npm i "gulpjs/gulp#4.0" -g
 npm i yarn -g
