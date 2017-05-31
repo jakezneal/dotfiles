a8540710bcc300c370327712515739e6524ca9e0
Jake Neal <hello@jakeneal.co.uk>
2017-05-31 22:28:33 +0100

    Install Oh My Zsh last

--- .misc.sh
+++ .misc.sh
@@ -1,8 +1,5 @@
 # Clone git repos & install misc tools
 
-# Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 # Install Composer
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
@@ -13,3 +10,6 @@ php -r "unlink('composer-setup.php');"
 # Install Laravel
 composer global require "laravel/installer"
 export PATH="$PATH:$HOME/.composer/vendor/bin"
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
