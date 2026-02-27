79eff8384a4a73f5cf67846afcb9329659720300
Jake Neal <hello@jakeneal.co.uk>
2026-02-27 18:08:11 +0000

    Refactor Homebrew installation and package lists
    Updated Homebrew installation method and modified package lists by removing and adding several packages.

--- install/brew.sh
+++ install/brew.sh
@@ -1,26 +1,17 @@
 # Install Homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-brew tap caskroom/cask
-brew tap caskroom/fonts
-brew tap caskroom/drivers
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew update
 brew upgrade
 
 brew=(
-    ack
     composer
     dockutil
-    emojify
     git
-    graphicsmagick
     mysql
     openssl
     python3
-    webkit2png
     wget
-    wifi-password
 )
 
 # Install packages
@@ -29,64 +20,29 @@ for package in "${brew[@]}"; do
 done;
 
 cask=(
-    1clipboard
     1password
-    adobe-creative-cloud
-    alfred
     appcleaner
-    astro
-    balsamiq-mockups
-    cyberduck
-    dropbox
-    firefox
-    flux
-    google-chrome
-    google-chrome-canary
-    imageoptim
-    iterm2
-    karabiner-elements
+    arc
+    dbngin
+    figma
+    flux-app
+    github
     keka
-    logitech-options
-    macdown
-    mamp
-    muzzle
-    quitter
-    rocket
+    logitune
+    maccy
+    notion-calendar
+    postman
+    raycast
+    rectangle
     slack
-    spectacle
     spotify
     tableplus
-    tower
     visual-studio-code
+    warp
+    whatsapp
 )
 
 # Install cask packages
 for package in "${cask[@]}"; do
     brew cask install --appdir="/Applications" $package
-done;
-
-quicklook=(
-    qlcolorcode
-    qlimagesize
-    qlmarkdown
-    qlstephen
-    qlvideo
-    quicklook-json
-    quicklook-csv
-    webpquicklook
-)
-
-# Install QL plugins
-for package in "${quicklook[@]}"; do
-    brew cask install $package
-done;
-
-fonts=(
-    inconsolata
-    fira-code
-)
-
-# Install fonts
-for font in "${fonts[@]}"; do
-    brew cask instal font-$font
 done;
