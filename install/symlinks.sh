9e031545f143ac49a2a4363124afb6e896fc3e35
Jake Neal <hello@jakeneal.co.uk>
2026-03-01 14:10:56 +0000

    🎨 Updated to use zshrc

--- install/symlinks.sh
+++ install/symlinks.sh
@@ -4,5 +4,5 @@ ln -sfv "$DOTFILES_DIR/dots/.extra" ~
 ln -sfv "$DOTFILES_DIR/dots/.aliases" ~
 ln -sfv "$DOTFILES_DIR/dots/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/dots/.gitignore" ~
-ln -sfv "$DOTFILES_DIR/dots/.bash_profile" ~
+ln -sfv "$DOTFILES_DIR/dots/.zshrc" ~
 ln -sfv "$DOTFILES_DIR/dots/z.sh" ~
